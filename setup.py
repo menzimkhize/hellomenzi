4a566e466783f428d42be7a7ac671e6c5477f14d
menzimkhize <43467504@mylife.unisa.ac.za>
2017-12-13 22:23:26 +0200

    first commit

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python
-
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='hellomenzi',
       version='1.0',
-      description='Python Distribution Utilities',
-      author='Greg Ward',
-      author_email='menzi.mail@gmail.com',
-      url='https://www.python.org/sigs/distutils-sig/',
-      packages=['distutils', 'distutils.command'],
-      executables = [Executable("hellomenzi.py")]
+      py_modules=['hellomenzi']
      )
 
